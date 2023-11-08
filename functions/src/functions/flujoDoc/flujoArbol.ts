9e16507c7a4a4583c96eda71ba5442a72bb3e6cf
miguel.gutierrez6040833 <miguel.gutierrez@vaku.cl>
2023-11-08 11:05:29 -0300

    feat: Limpieza de codigo

--- functions/src/functions/flujoDoc/flujoArbol.ts
+++ functions/src/functions/flujoDoc/flujoArbol.ts
@@ -5,16 +5,6 @@ import { convertDocumentTimestampsToDate } from '../../core/utils';
 import { procesarDocumentoFlujoGenerar } from '../../core/caseUse/generarDoc';
 import { FirestoreRepository } from '../../core/services/repository/FirestoreRepository';
 
-export const generarNuevoDocumento = onDocumentCreated("empresas/{nombreEmpresa}/gerencias/{nombreGerencia}/divisiones/{nombreDivision}/documentos/{docId}", async(event)  => {
-    const data = event.data?.data();
-    const transformedData = convertDocumentTimestampsToDate(data);
-    const doc = plainToClass(Documento, transformedData);
-    console.log("--------caso de uso ----->");
-    const rutaDoc = `empresas/${event.params.nombreEmpresa}/gerencias/${event.params.nombreGerencia}/divisiones/${event.params.nombreDivision}/documentos`;
-    const repo = new FirestoreRepository<Documento>(rutaDoc);
-    const empresa = event.params.nombreEmpresa;
-    await procesarDocumentoFlujoGenerar(doc,repo,empresa);
-})
 export const flujoNuevoDocumentoConArboles = onDocumentCreated("empresas/{nombreEmpresa}/gerencias/{nombreGerencia}/divisiones/{nombreDivision}/documentos/{docId}", async(event)  => {
     const data = event.data?.data();
     const transformedData = convertDocumentTimestampsToDate(data);
